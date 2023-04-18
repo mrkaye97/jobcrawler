afff9d9dfebb7d84706f6c2b236abd37abb25c82
mrkaye97 <mrkaye97@gmail.com>
2023-04-17 23:57:38 -0400

    adds email send using sendinblue

--- jobcrawler/app.py
+++ jobcrawler/app.py
@@ -5,6 +5,8 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from bs4 import BeautifulSoup
 import requests
 from urllib.parse import urljoin
+import sib_api_v3_sdk
+import os
 
 app = Flask(__name__)
 
@@ -51,11 +53,25 @@ def erase(id):
     db.session.commit()
     return jsonify(f"Successfully deleted {id}")
 
-cron_time = '0 * * * *'
-
 sched = BackgroundScheduler()
 
-@sched.scheduled_job(trigger = 'interval', seconds = 10, id='crawl')
+def send_email(sender_name, sender_email, recipient, subject, body):
+    configuration = sib_api_v3_sdk.Configuration()
+    configuration.api_key['api-key'] = os.environ.get("SIB_API_KEY")
+
+    # create an instance of the API class
+    api_instance = sib_api_v3_sdk.AccountApi(sib_api_v3_sdk.ApiClient(configuration))
+    sender = sib_api_v3_sdk.SendSmtpEmailSender(name = sender_name, email = sender_email)
+    to = [sib_api_v3_sdk.SendSmtpEmailTo(email = recipient)]
+
+    api_instance = sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(configuration))
+    send_smtp_email = sib_api_v3_sdk.SendSmtpEmail(sender = sender, to = to, subject = subject, text_content = body)
+
+    response = api_instance.send_transac_email(send_smtp_email)
+
+    print(response)
+
+@sched.scheduled_job(trigger = 'interval', seconds = 3, id='crawl')
 def crawl():
     """ Function for test purposes. """
     print("Scheduler is alive!")
@@ -64,22 +80,34 @@ def crawl():
         searches = db.session.query(Boards.company, Boards.url, Boards.search_text)
 
         for search in searches:
-            print("Company: ", search.company)
-            print("URL: ", search.url)
-            print("Text: ", search.search_text)
-
             r = requests.get(search.url)
             soup = BeautifulSoup(r.content, features="html.parser")
 
+            print(search.search_text.lower())
             links = lambda tag: (getattr(tag, 'name', None) == 'a' and
                                     'href' in tag.attrs and
                                     search.search_text.lower() in tag.get_text().lower())
 
             links = soup.find_all(links)
             links = [urljoin(search.url, tag['href']) for tag in links]
-            results = results + links
 
-    print(results)
+            if links:
+                links = ", ".join(links)
+                links = [f"{search.company} -- {search.search_text}: {links}"]
+                results = results + links
+
+    result_txt = "\n".join(results)
+    send_email(
+        sender_email = "mrkaye97@gmail.com",
+        sender_name = "Matt Kaye",
+        recipient = "matt.kaye@collegevine.com",
+        subject = "Your daily job feed digest",
+        body = f"""
+            Here are your links for the day!
+
+            {result_txt}
+        """
+    )
 
 sched.start()
 
