1f98b25bfb1fcc90e060f70d9c0f6174e49e881b
mrkaye97 <mrkaye97@gmail.com>
2023-04-19 18:26:52 -0400

    add a user id to searches + add indexes

--- src/models.py
+++ src/models.py
@@ -5,9 +5,9 @@ db = SQLAlchemy()
 
 class Searches(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    company_id = db.Column(db.Integer, db.ForeignKey("companies.id"))
-
+    company_id = db.Column(db.Integer, db.ForeignKey("companies.id"), index = True)
     search_text = db.Column(db.String(256), unique=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), index = True)
 
     def __repr__(self):
         return f"Company : {self.company}, URL: {self.url}, search: {self.search_text}"
