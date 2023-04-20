1085d7e670daf90e772e090b635b61655b0c1211
mrkaye97 <mrkaye97@gmail.com>
2023-04-20 00:05:47 -0400

    dynamically get db url

--- config.py
+++ config.py
@@ -2,5 +2,9 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/jobcrawler'
+    if os.environ.get("ENV") == "PROD":
+        SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    else:
+        SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/jobcrawler'
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
