c1654f108c50f6530f18420be665090a531cdc46
mrkaye97 <mrkaye97@gmail.com>
2023-04-21 21:07:53 -0400

    regex migration

--- src/models.py
+++ src/models.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 class Searches(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     company_id = db.Column(db.Integer, db.ForeignKey("companies.id"), index = True)
-    search_regex = db.Column(db.String(256), unique=False, nullable=False)
+    search_regex = db.Column(db.String(256), unique=False)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), index = True)
     posting_id = db.Column(db.Integer, db.ForeignKey("postings.id"))
 
