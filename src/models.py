db2f5aa57b0490e01c5f252a3e41fa378540de67
mrkaye97 <mrkaye97@gmail.com>
2023-04-29 14:29:16 -0400

    expand company url fields

--- src/models.py
+++ src/models.py
@@ -27,8 +27,8 @@ class Users(UserMixin, db.Model):
 class Companies(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(128), index = True, nullable = False)
-    board_url = db.Column(db.String(128), nullable = False)
-    job_posting_url_prefix = db.Column(db.String(128), nullable = True)
+    board_url = db.Column(db.String(2048), nullable = False)
+    job_posting_url_prefix = db.Column(db.String(2048), nullable = True)
     scraping_method = db.Column(db.String(64), nullable = False, default = "soup")
 
     def __repr__(self):
