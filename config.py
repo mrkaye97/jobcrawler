69b1cce3bb74d322525413edfb989e4a4bb067bd
mrkaye97 <mrkaye97@gmail.com>
2023-04-30 14:21:18 -0400

    small config optimization

--- config.py
+++ config.py
@@ -2,9 +2,5 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    if os.environ.get("ENV") == "PROD":
-        SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    else:
-        SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/jobcrawler'
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
