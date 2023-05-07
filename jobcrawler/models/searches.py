bb7c68f838cb97699dc0b17142eadf9797f90da2
mrkaye97 <mrkaye97@gmail.com>
2023-05-07 14:11:24 -0400

    FK both the search and the posting to a company, but not each other

--- jobcrawler/models/searches.py
+++ jobcrawler/models/searches.py
@@ -5,7 +5,6 @@ class Searches(db.Model):
     company_id = db.Column(db.Integer, db.ForeignKey("companies.id"), index = True)
     search_regex = db.Column(db.String(256), unique=False)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), index = True)
-    posting_id = db.Column(db.Integer, db.ForeignKey("postings.id"))
 
     def __repr__(self):
         return f"Company : {self.company}, URL: {self.url}, search: {self.search_regex}"
