a2fda595906d39af8ac23b15113440f70f9decf4
mrkaye97 <mrkaye97@gmail.com>
2023-05-07 20:53:39 -0400

    adding more tests

--- tests/unit/test_scraping.py
+++ tests/unit/test_scraping.py
@@ -1,6 +1,9 @@
-from jobcrawler.jobs.scraping import load_page, create_posting_advertisement, is_matching_posting
+from jobcrawler.jobs.scraping import load_page, create_posting_advertisement, is_matching_posting, get_users_to_email
 from jobcrawler.exceptions import ScrapingException
+from jobcrawler import db
 import pytest
+
+from jobcrawler.models.users import Users
 
 def test_load_page_raises_on_404():
     with pytest.raises(ScrapingException):
@@ -35,7 +38,25 @@ def test_posting_ad_creation():
 
     ad = create_posting_advertisement(title, company, href)
 
-    assert title in ad
-    assert "@" in ad
-    assert company in ad
-    assert href in ad
+    assert title in ad.get("text")
+    assert "@" in ad.get("text")
+    assert company in ad.get("text")
+    assert href == ad.get("href")
+
+def test_users_to_email(app):
+    u1 = Users(email = "kaye.dev", email_frequency_days = 1)
+    u2 = Users(email = "mk.dev", email_frequency_days = 1000000)
+
+    db.session.add(u1)
+    db.session.add(u2)
+    db.session.commit()
+
+    emails = [u.email for u in get_users_to_email()]
+
+    assert "kaye.dev" in emails
+    assert "mk.dev" not in emails
+
+    db.session.delete(u1)
+    db.session.delete(u2)
+
+    db.session.commit()
