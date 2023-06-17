3d0db878c326eee0ac11a74c400b9f4a01911bd2
mrkaye97 <mrkaye97@gmail.com>
2023-06-17 11:25:16 -0400

    ignore warning from apsched

--- jobcrawler/extensions/scheduler.py
+++ jobcrawler/extensions/scheduler.py
@@ -1,3 +1,10 @@
+import warnings
+
+warnings.filterwarnings(
+    "ignore",
+    message="The localize method is no longer necessary, as this time zone supports the fold attribute",
+)
+
 from flask_apscheduler import APScheduler
 
 sched = APScheduler()
